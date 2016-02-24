5b46c8f00e51d6379b23ae1eb1b76fe21828bb4d
decipher <vitaliy.maltsev@gmail.com>
2016-02-24 15:51:59 +0300

    removed dist from gitignore

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var http = require('http');
 
 app.use(morgan('dev'));
 //app.use(gzippo.staticGzip("" + __dirname + "/dist"));
