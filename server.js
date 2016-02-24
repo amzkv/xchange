8c543488d042cd98a38736370312468520d564c9
decipher <vitaliy.maltsev@gmail.com>
2016-02-24 15:26:38 +0300

    express static

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ var app = express();
 
 app.use(morgan('dev'));
 //app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.use(express.static(__dirname + + "/dist"));
+app.use(express.static(__dirname + "/dist"));
 
 app.get('/', function(req, res) {
 
   // ejs render automatically looks in the views folder
-  res.sendFile("index.html");
+  res.sendFile(__dirname + "/dist/index.html");
 });
 
 app.listen(process.env.PORT || 5000);
