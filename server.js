3bef95c81a7f0c40a5e000739435b8c3278f086e
decipher <vitaliy.maltsev@gmail.com>
2016-02-24 15:16:59 +0300

    express static

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ var app = express();
 
 app.use(morgan('dev'));
 //app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.use(express.static('dist'));
+app.use(express.static(__dirname + + "/dist"));
 app.listen(process.env.PORT || 5000);
