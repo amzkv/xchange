0679298a3c309635ca4723a2be8d7e2c00228977
Vitaly Maltsev <vitaly.maltsev@elinext.com>
2016-05-13 17:07:10 +0300

    build

--- src/app/components/validateEmail/validateEmail.directive.js
+++ src/app/components/validateEmail/validateEmail.directive.js
@@ -1,25 +1,6 @@
 /**
  * Created by decipher on 11.5.16.
  */
-/*directive('validateEmail', function() {
-  var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-
-  return {
-    require: 'ngModel',
-    restrict: '',
-    link: function(scope, elm, attrs, ctrl) {
-      // only apply the validator if ngModel is present and Angular has added the email validator
-      if (ctrl && ctrl.$validators.email) {
-
-        // this will overwrite the default Angular email validator
-        ctrl.$validators.email = function(modelValue) {
-          return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
-        };
-      }
-    }
-  };
-});*/
-
 export function ValidateEmail() {
   'ngInject';
 
