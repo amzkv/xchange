fc5af8665176330690af950b5ceb4f6b2e24bebd
amazakov <amazakov@elilink.com>
2016-05-17 17:25:13 +0300

    onsen ui fixes and updates

--- gulp/build.js
+++ gulp/build.js
@@ -85,13 +85,6 @@ gulp.task('fonts', function () {
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
 });
 
-gulp.task('onsenfix', function () {
-  var onsenPath = 'bower_components/OnsenUI';
-  return gulp.src([path.join(onsenPath, '/**/*')])
-    .pipe($.filter(['css/**/*','stylus/**/*','js/**/*']))
-    .pipe(gulp.dest(path.join(onsenPath, '/build/')));
-});
-
 gulp.task('other', function () {
   var fileFilter = $.filter(function (file) {
     return file.stat.isFile();
@@ -109,4 +102,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other', 'onsenfix']);
+gulp.task('build', ['html', 'fonts', 'other']);
