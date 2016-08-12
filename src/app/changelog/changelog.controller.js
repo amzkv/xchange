0f7ff17a56d5a342a22928e93b340ba1abf78dd8
amazakov <amazakov@elilink.com>
2016-08-12 12:50:53 +0300

    clear all collections local data

--- src/app/changelog/changelog.controller.js
+++ src/app/changelog/changelog.controller.js
@@ -2,12 +2,14 @@
  * Created by decipher on 8.6.16.
  */
 export class ChangeLogController {
-  constructor ($scope, LocalAccessService, $state) {
+  constructor ($scope, LocalAccessService, documentsService, $state) {
     'ngInject';
     this.localAccesService = LocalAccessService;
+    this.documentsService = documentsService;
     let self = this;
     $scope.clearLocalData = function() {
       self.localAccesService.clearLocalData();
+      self.documentsService.allCollections = null;
       $state.go('login');
     }
   }
