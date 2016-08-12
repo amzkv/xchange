dc7848460f79f194a1a57bb4633e093f203721c8
amazakov <amazakov@elilink.com>
2016-08-12 15:05:10 +0300

    changelog redirect fix

--- src/app/changelog/changelog.controller.js
+++ src/app/changelog/changelog.controller.js
@@ -2,13 +2,14 @@
  * Created by decipher on 8.6.16.
  */
 export class ChangeLogController {
-  constructor ($scope, LocalAccessService, documentsService, $state) {
+  constructor ($scope, $rootScope, LocalAccessService, documentsService, $state) {
     'ngInject';
     this.localAccesService = LocalAccessService;
     this.documentsService = documentsService;
     let self = this;
     $scope.clearLocalData = function() {
       self.localAccesService.clearLocalData();
+      $rootScope.loggedOut = true;
       self.documentsService.allCollections = null;
       $state.go('login');
     }
